c96e33f90ce313de7ad50392003c7e3bbef1f20e
Peng Liu <pengliu.mail@gmail.com>
2018-02-28 00:39:48 -0600

    Ran expand to replace tabs with spaces

--- cudaMappedMemory.h
+++ cudaMappedMemory.h
@@ -33,20 +33,20 @@
  */
 inline bool cudaAllocMapped( void** cpuPtr, void** gpuPtr, size_t size )
 {
-	if( !cpuPtr || !gpuPtr || size == 0 )
-		return false;
+  if( !cpuPtr || !gpuPtr || size == 0 )
+    return false;
 
-	//CUDA(cudaSetDeviceFlags(cudaDeviceMapHost));
+  //CUDA(cudaSetDeviceFlags(cudaDeviceMapHost));
 
-	if( CUDA_FAILED(cudaHostAlloc(cpuPtr, size, cudaHostAllocMapped)) )
-		return false;
+  if( CUDA_FAILED(cudaHostAlloc(cpuPtr, size, cudaHostAllocMapped)) )
+    return false;
 
-	if( CUDA_FAILED(cudaHostGetDevicePointer(gpuPtr, *cpuPtr, 0)) )
-		return false;
+  if( CUDA_FAILED(cudaHostGetDevicePointer(gpuPtr, *cpuPtr, 0)) )
+    return false;
 
-	memset(*cpuPtr, 0, size);
-	printf("[cuda]  cudaAllocMapped %zu bytes, CPU %p GPU %p\n", size, *cpuPtr, *gpuPtr);
-	return true;
+  memset(*cpuPtr, 0, size);
+  printf("[cuda]  cudaAllocMapped %zu bytes, CPU %p GPU %p\n", size, *cpuPtr, *gpuPtr);
+  return true;
 }
 
 
--- cudaUtility.h
+++ cudaUtility.h
@@ -35,25 +35,25 @@
  * @return the original cudaError_t result
  * @ingroup util
  */
-#define CUDA(x)				cudaCheckError((x), #x, __FILE__, __LINE__)
+#define CUDA(x)       cudaCheckError((x), #x, __FILE__, __LINE__)
 
 /**
  * Evaluates to true on success
  * @ingroup util
  */
-#define CUDA_SUCCESS(x)			(CUDA(x) == cudaSuccess)
+#define CUDA_SUCCESS(x)     (CUDA(x) == cudaSuccess)
 
 /**
  * Evaluates to true on failure
  * @ingroup util
  */
-#define CUDA_FAILED(x)			(CUDA(x) != cudaSuccess)
+#define CUDA_FAILED(x)      (CUDA(x) != cudaSuccess)
 
 /**
  * Return from the boolean function if CUDA call fails
  * @ingroup util
  */
-#define CUDA_VERIFY(x)			if(CUDA_FAILED(x))	return false;
+#define CUDA_VERIFY(x)      if(CUDA_FAILED(x))  return false;
 
 /**
  * LOG_CUDA string.
@@ -75,25 +75,25 @@
 inline cudaError_t cudaCheckError(cudaError_t retval, const char* txt, const char* file, int line )
 {
 #if !defined(CUDA_TRACE)
-	if( retval == cudaSuccess)
-		return cudaSuccess;
+  if( retval == cudaSuccess)
+    return cudaSuccess;
 #endif
 
-	//int activeDevice = -1;
-	//cudaGetDevice(&activeDevice);
+  //int activeDevice = -1;
+  //cudaGetDevice(&activeDevice);
 
-	//Log("[cuda]   device %i  -  %s\n", activeDevice, txt);
-	
-	printf(LOG_CUDA "%s\n", txt);
+  //Log("[cuda]   device %i  -  %s\n", activeDevice, txt);
+  
+  printf(LOG_CUDA "%s\n", txt);
 
 
-	if( retval != cudaSuccess )
-	{
-		printf(LOG_CUDA "   %s (error %u) (hex 0x%02X)\n", cudaGetErrorString(retval), retval, retval);
-		printf(LOG_CUDA "   %s:%i\n", file, line);	
-	}
+  if( retval != cudaSuccess )
+  {
+    printf(LOG_CUDA "   %s (error %u) (hex 0x%02X)\n", cudaGetErrorString(retval), retval, retval);
+    printf(LOG_CUDA "   %s:%i\n", file, line);  
+  }
 
-	return retval;
+  return retval;
 }
 
 
@@ -101,7 +101,7 @@ inline cudaError_t cudaCheckError(cudaError_t retval, const char* txt, const cha
  * iDivUp
  * @ingroup util
  */
-inline __device__ __host__ int iDivUp( int a, int b )  		{ return (a % b != 0) ? (a / b + 1) : (a / b); }
+inline __device__ __host__ int iDivUp( int a, int b )     { return (a % b != 0) ? (a / b + 1) : (a / b); }
 
 
 
